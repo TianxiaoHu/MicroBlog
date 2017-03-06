c875ce809856bc6e7c274e4f17c1a4987af399a8
TianxiaoHu <hutianxiao_fdu@126.com>
2017-03-06 23:42:42 +0800

    User Info & Avatar
    - create a user info page
    - show avatars based on Gravatar
    - add new features for user info and upgrade the database
    - enable users to edit their profiles

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import TextField, BooleanField, StringField, TextAreaField
+from wtforms.validators import Required, DataRequired, Length
 
 class LoginForm(FlaskForm):
     email = TextField('email', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
+
+class EditForm(FlaskForm):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
